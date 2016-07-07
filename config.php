23c3a363c93b787e0920598fd9ef3aea2ebbc817
Andrey Danil <adanilko@hawk.iit.edu>
2016-07-06 22:40:40 -0500

    updating config

--- config.php
+++ config.php
@@ -1,7 +1,18 @@
 <?php
 
 // Facebook Application ID
-define('FACEBOOK_APP_ID', '');
+define('FACEBOOK_APP_ID', 'ADD_FACEBOOK_APP_ID_HERE');
 
 // Facebook Application Secret
-define('FACEBOOK_APP_SECRET', '');
+define('FACEBOOK_APP_SECRET', 'ADD_FACEBOOK_APP_SECRET_HERE'}});
+
+/*
+
+Installation:
+--------------------
+
+1- Create Facebook Application from https://developers.facebook.com/quickstarts/?platform=web
+2- Open config.php and update app id and secret to yours.
+3- upload the source folder and run it from the browser.
+
+*/
